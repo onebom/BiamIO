203c5d4d2c6205913404a535b496d41c8b8eb562
phwGithub <yey6689@gmail.com>
2023-03-02 05:53:25 +0900

    max speed set

--- src-flask-server/app.py
+++ src-flask-server/app.py
@@ -318,7 +318,7 @@ class SnakeGameClass:
 
         self.speed = 5
         self.minspeed = 10
-        self.maxspeed = math.hypot(1280, 720) / 10
+        self.maxspeed = 50
         self.velocityX = random.choice([-1, 0, 1])
         self.velocityY = random.choice([-1, 1])
 
@@ -357,7 +357,7 @@ class SnakeGameClass:
 
         self.speed = 5
         self.minspeed = 10
-        self.maxspeed = math.hypot(1280, 720) / 10
+        self.maxspeed = 50
         self.velocityX = random.choice([-1, 0, 1])
         self.velocityY = random.choice([-1, 1])
 
@@ -1164,7 +1164,6 @@ def test():
                 pointIndex = [cx, cy]
             else:
                 if hands:
-                    print()
                     lmList = hands[0]['lmList']
                     pointIndex = lmList[8][0:2]
 
